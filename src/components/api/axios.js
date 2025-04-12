95a44c0c1f14b55b630fb3e9902dc776f708ec58
macdallas <roylexstephane@gmail.com>
2025-04-12 03:27:59 +0100

    Use Online api

--- src/components/api/axios.js
+++ src/components/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
    //baseURL: "http://localhost:5000/api/v1"
-   baseURL: "http://localhost:5000/api/v1"
+   baseURL: "https://ing.gabx.api/api/v1"
 })
