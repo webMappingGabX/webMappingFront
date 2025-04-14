6570eea7e185179da5e9df808d66822e27877953
macdallas <roylexstephane@gmail.com>
2025-04-14 23:42:35 +0100

    setup for production

--- src/components/api/axios.js
+++ src/components/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-   baseURL: "http://localhost:5000/api/v1"
-   //baseURL: "https://vitreous-elk-digitalmaking-719d936f.koyeb.app/api/v1"
+   //baseURL: "http://localhost:5000/api/v1"
+   baseURL: "https://vitreous-elk-digitalmaking-719d936f.koyeb.app/api/v1"
 })
