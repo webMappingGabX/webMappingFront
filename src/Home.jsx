14ec1200ac646995e7c6a73875cb8dc7a77afd3c
macdallas <roylexstephane@gmail.com>
2025-04-20 11:01:05 +0100

    Add Features

--- src/Home.jsx
+++ src/Home.jsx
@@ -825,6 +825,12 @@ const Home = () => {
 
     const geoJSON = {
       type: "FeatureCollection",
+      crs: {
+        type: "name",
+        properties: {
+          name: "urn:ogc:def:crs:OGC:1.3:CRS84" // WGS84 projection system
+        }
+      },
       features: features,
     };
 
